d3015a97dc3efd2db9ea37060f11a84cb3c51306
adonai-luque <akluqueprieto@gmail.com>
2020-04-22 04:40:41 -0500

    Indentation corrected

--- week-1/day-2/kaprekars_number.rb
+++ week-1/day-2/kaprekars_number.rb
@@ -1,6 +1,6 @@
 def kaprekar?(k)
-    digits = Math.log10(k).ceil
-    (k ** 2) / (10 ** digits) + (k ** 2) % (10 ** digits) == k
+  digits = Math.log10(k).ceil
+  (k ** 2) / (10 ** digits) + (k ** 2) % (10 ** digits) == k
 end
 
 puts kaprekar?(9)
